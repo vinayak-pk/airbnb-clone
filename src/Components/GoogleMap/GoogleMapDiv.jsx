5e1293d9f373d54746d93b4c11c18dbb77f9f539
meera <shu@ahb.com>
2021-06-04 00:56:18 +0530

    update google map with price

--- src/Components/GoogleMap/GoogleMapDiv.jsx
+++ src/Components/GoogleMap/GoogleMapDiv.jsx
@@ -1,21 +1,44 @@
-import React from 'react'
-import { GoogleMap, withGoogleMap, withScriptjs,Marker } from "react-google-maps";
+import React, { useState } from 'react'
+import { GoogleMap, withGoogleMap, withScriptjs, Marker, InfoWindow } from "react-google-maps";
 import hotel from "../../Data/hotel.json"
 
 const GoogleMapDiv = () => {
+  const [list, setList] = useState(null)
   return (
-      <GoogleMap defaultZoom={10} defaultCenter={{lat:26.808790,lng:80.959085}}>
+    <GoogleMap defaultZoom={10} defaultCenter={{ lat: 26.808790, lng: 80.959085 }}>
+      {
+        hotel.map((location) =>
+        (
+          <Marker key={location.id}
+            position={{
+              lat: location.geometry.coordinate[0],
+              lng: location.geometry.coordinate[1]
+            }}
+            onClick={() => {
+              setList(location)
+            }} />
+        ))
+      }
+      {list && (
+        <InfoWindow position={{
+          lat: list.geometry.coordinate[0],
+          lng: list.geometry.coordinate[1]
+        }}
+        onCloseClick={()=>
         {
-          hotel.map((location)=>
-          (
-            <Marker key ={location.id} position={{lat:location.geometry.coordinate[0],lng:location.geometry.coordinate[1]}}/>
-          ))
-        }
-      </GoogleMap>
+          setList(null)
+        }}
+        >
+          <div>
+            {list.price}$
+             </div>
+        </InfoWindow>
+      )}
+    </GoogleMap>
 
   )
 }
-export const WrappedMap=withScriptjs(withGoogleMap(GoogleMapDiv))
+export const WrappedMap = withScriptjs(withGoogleMap(GoogleMapDiv))
 
 export default GoogleMapDiv
 
