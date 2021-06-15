594267925f7792529e165db5a83135f9febaa401
Ashutosh Pandey <rojha2030@gmail.com>
2021-06-15 21:15:55 +0530

    hoist_page

--- src/Components/HoistingProperty/HostPlace/HostPlace.jsx
+++ src/Components/HoistingProperty/HostPlace/HostPlace.jsx
@@ -2,8 +2,45 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 import Logo2 from "../Pictures/Logo2.svg";
 import "./Hostplace.css";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+import Tooltip from "@material-ui/core/Tooltip";
+import EditLocationIcon from "@material-ui/icons/EditLocation";
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  geocodeByPlaceId,
+  getLatLng,
+} from "react-places-autocomplete";
 
-const HostPlace = () => {
+const useStylessss = makeStyles(() => ({
+  noBorder: {
+    border: "none",
+    borderRightStyle: "solid",
+    borderRightColor: "grey",
+    backgroundColor: "white",
+  },
+}));
+
+const HostPlace = ({ setTempVal, tempVal }) => {
+  const classesified = useStylessss();
+  const [address, setAddress] = React.useState("");
+  const [coordinate, setcoordinates] = React.useState({
+    lat: null,
+    lng: null,
+  });
+
+  const handleSelect = async (value) => {
+    const results = await geocodeByAddress(value);
+    const ll = await getLatLng(results[0]);
+    console.log(ll);
+    setAddress(value);
+    setcoordinates(ll);
+    // setTempVal({ ...tempVal, location: ll });
+  };
+  const locations = (coordinate) => {
+    setTempVal({ ...tempVal, location: { ...coordinate } });
+  };
+
   return (
     <div className="containerHostPlace">
       <div className="dataContainer">
@@ -23,8 +60,83 @@ const HostPlace = () => {
             <Button variant="outlined">Help</Button>
             <Button variant="outlined">Save and exit</Button>
           </div>
-          <div>
-            
+          <div className="inputfielCont">
+            <PlacesAutocomplete
+              value={address}
+              onChange={setAddress}
+              onSelect={handleSelect}
+            >
+              {({
+                getInputProps,
+                suggestions,
+                getSuggestionItemProps,
+                loading,
+              }) => (
+                <div>
+                  <Tooltip title="Where are you going?" arrow>
+                    <input
+                      id="standard-basic"
+                      disableUnderline={true}
+                      // value={tempVal.location}
+                      margin="normal"
+                      label="Locations"
+                      onChange={() => locations(coordinate)}
+                      variant="outlined"
+                      InputProps={{
+                        classes: { notchedOutline: classesified.noBorder },
+                      }}
+                      {...getInputProps({
+                        placeholder: "Enter your address ...",
+                        className: "location-search-input",
+                      })}
+                    />
+                  </Tooltip>
+                  <div className="autocomplete-dropdown-container">
+                    {loading && <div>Loading...</div>}
+                    {suggestions.map((suggestion) => {
+                      const className = suggestion.active
+                        ? "suggestion-item--active"
+                        : "suggestion-item";
+                      // inline style for demonstration purpose
+                      const style = suggestion.active
+                        ? {
+                            backgroundColor: "#e7e7e7",
+                            cursor: "pointer",
+                            color: "black",
+                            // borderRadius: "10px",
+                            width: "500px",
+                            padding: "20px",
+                            borderBottom: "1px solid grey",
+                            display: "flex",
+                            justifyContent: "space-between",
+                          }
+                        : {
+                            backgroundColor: "#ffffff",
+                            color: "black",
+                            // borderRadius: "10px",
+                            width: "500px",
+                            padding: "20px",
+                            display: "flex",
+                            justifyContent: "space-between",
+                            // borderBottom: "1px solid grey",
+                          };
+                      return (
+                        <div
+                          {...getSuggestionItemProps(suggestion, {
+                            className,
+                            style,
+                          })}
+                        >
+                          {" "}
+                          <EditLocationIcon />
+                          <span>{suggestion.description}</span>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              )}
+            </PlacesAutocomplete>
           </div>
           <div className="footerCont">
             <Button variant="outlined">Back</Button>
