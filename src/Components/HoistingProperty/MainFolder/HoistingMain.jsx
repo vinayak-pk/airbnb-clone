fe16dd28962f03b75cb28986c3a700e11a6c83f9
Ashutosh Pandey <rojha2030@gmail.com>
2021-06-16 22:24:24 +0530

    .

--- src/Components/HoistingProperty/MainFolder/HoistingMain.jsx
+++ src/Components/HoistingProperty/MainFolder/HoistingMain.jsx
@@ -2,14 +2,14 @@ import React from "react";
 // import NumGuest from "../NumOfGuest/NumGuest";
 // import HostPlace from "../HostPlace/HostPlace";
 // import HostAddress from "../HostAddress/HostAddress";
-import PropertyType from "../PropertyType/PropertyType";
+// import PropertyType from "../PropertyType/PropertyType";
 const HoistingMain = () => {
   return (
     <div>
       {/* <HostPlace /> */}
       {/* <HostAddress/> */}
       {/* <NumGuest /> */}
-      <PropertyType />
+      {/* <PropertyType /> */}
     </div>
   );
 };
