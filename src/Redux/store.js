538adc2ba6eaa85340c04d2695674a80f7ca0457
Ashutosh Pandey <rojha2030@gmail.com>
2021-08-23 22:15:58 +0530

    .

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -13,7 +13,7 @@ const rootReducer=combineReducers({
 export const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
